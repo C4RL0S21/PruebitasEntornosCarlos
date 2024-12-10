57bb430143de8c94d53b2f0f41f3ea0a89434719
C4RL0S21 <cm4291@gmail.com>
2024-12-10 19:40:45 +0100

    Add files via upload

---
+++ ControErrores.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ * Introduce una frase fura de un rango
+ *
+ * @author Diurno
+ */
+public class ControErrores {
+
+    public static void main(String[] args) {
+//        Scanner reader = new Scanner(System.in);
+//
+////        System.out.println("Introduce un numero");
+//        int numero = reader.nextInt();
+//        
+//        System.out.println(numero);
+//        
+//        System.out.println("Fin programa");
+//        
+//        try{
+//        System.out.println(5/0);//supceptible de error
+//        } catch (Exception e){
+//            System.out.println("No es posible divir enre 0");
+////        }
+//        String frase = "";
+//        System.out.println("Dime una frase");
+//        frase = reader.nextLine();
+//
+//        try {
+//            System.out.println(frase.substring(4, 10));//supceptible de error
+//            catch (StringIndexOutOfBoundsException sioobe) {
+//            System.out.println("Te has silido de la frase");
+//                    System.out.println(5*4);
+//        }
+//        } catch (Exception e) {
+//            System.out.println("Error desconocido" + e);
+//        }
+  }
+}
